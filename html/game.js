7e9bb6ee4d24bbefc1011006da1d5ff2a018c6a8
Oscar Bullen <60191312+green-eggs-and-ham@users.noreply.github.com>
2022-07-18 17:57:24 +0100

    added `bash` functionality and graphical flavor

--- html/game.js
+++ html/game.js
@@ -11,6 +11,7 @@ class paddle {
     this.ai=ai;
     this.min_y=14;
     this.max_y=242;
+    this.bash_tick=0;
     if (side==0) {
       this.min_x=8;
       this.max_x=248;
@@ -26,6 +27,16 @@ class paddle {
     stroke(255);
     strokeWeight(4);
     line(this.x,this.y-14,this.x,this.y+14);
+  }
+  
+  render_bash() {
+    noStroke();
+    fill(192);
+    if (this.side) {
+      rect(512,256,-1*(360-this.bash_tick)/360*250,16,0,5,5,0);
+    } else {
+      rect(0,256,(360-this.bash_tick)/360*250,16,0,5,5,0);
+    }
   }
   
   reset_coords() {
@@ -48,6 +59,12 @@ class paddle {
     }
   }
   
+  recharge(){
+    if (this.bash_tick){
+      this.bash_tick=this.bash_tick-1;
+    }
+  }
+  
   update_coords() {
     if (this.ai) {
       this.reset_coords();
@@ -55,47 +72,70 @@ class paddle {
       this.nx=0;
       this.ny=0;
       if (keyIsDown(87)){
-        this.ny=this.ny-this.ms;    
+        this.ny=this.ny-this.ms;
+        this.recharge();
       }
       if (keyIsDown(83)){
         this.ny=this.ny+this.ms;
+        this.recharge();
       }
       if (keyIsDown(65)){
         this.nx=this.nx-this.ms;
+        this.recharge();
       }
       if (keyIsDown(68)){
         this.nx=this.nx+this.ms;
+        this.recharge();
       }
       if (keyIsDown(32)){
-        print("bash");    
+        if (!this.bash_tick) {
+          this.bash_tick=360;
+        }
+      }
+      if (this.bash_tick > 345){
+        this.ms=this.ms*1.2;
+      } else {
+        this.ms=2;
       }
       this.check_coords(this.x+this.nx,this.y+this.ny);
       if (this.check_x){this.x=this.x+this.nx}
       if (this.check_y){this.y=this.y+this.ny}
     }
   }
-  
 }
 
 function setLineDash(list) {
   drawingContext.setLineDash(list);
 }
 
-
-
 function setup() {
-  createCanvas(512, 256);
+  angleMode(DEGREES);
+  frameRate(90);
+  createCanvas(512,256+16);
   left_paddle=new paddle();
-  right_paddle=new paddle(side=1);
+  right_paddle=new paddle(side=1,ai=0);
+  ticker=0;
 }
 
 function draw() {
-  background(14);
-  stroke(255);
+  ticker=ticker+1;
+  tick_multi=abs(sin(ticker));
+  tick_multi_2=abs(cos(ticker));
+  background(16*tick_multi,16*tick_multi,16*tick_multi,16*tick_multi_2+64);
+  stroke(128,128,128);
   strokeWeight(2);
-  setLineDash([5, 5]);
-  line(256,0,256,256);
+  setLineDash([5,5]);
+  line(256,0-tick_multi*8,256,256+tick_multi_2*8);
   left_paddle.render();
   right_paddle.render();
   left_paddle.update_coords();
+  right_paddle.update_coords();
+  fill(64);
+  noStroke();
+  rect(0,256,512,16);
+  fill(48);
+  rect(0,256,250,16,0,5,5,0);
+  rect(512,256,-250,16,0,5,5,0);
+  left_paddle.render_bash();
+  right_paddle.render_bash();
 }
